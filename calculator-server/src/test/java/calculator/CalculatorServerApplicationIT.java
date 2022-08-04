667988f83539910d96f76cb750e1841e74fcb709
Istvan Viczian <viczian.istvan@gmail.com>
2022-08-04 13:45:52 +0200

    Spring Boot

---
+++ calculator-server/src/test/java/calculator/CalculatorServerApplicationIT.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServerApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2cba225903e792591c5360003335fd77c3b31c9d
Istvan Viczian <viczian.istvan@gmail.com>
2022-08-05 09:47:39 +0200

    Init

---
+++ employees-rest-server/src/main/java/employees/EmployeesRestServerApplication.java
@@ -0,0 +1,13 @@
+package employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesRestServerApplication.class, args);
+	}
+
+}

8bd4f3cc16ad0ceb88913291e44c5d67d663effa
Istvan Viczian <viczian.istvan@gmail.com>
2022-08-05 10:03:46 +0200

    Kezdet

---
+++ employees-rest-server/src/main/java/employees/EmployeesService.java
@@ -0,0 +1,60 @@
+package employees;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeesService {
+
+    private EmployeeMapper employeeMapper;
+
+    private final AtomicLong idGenerator = new AtomicLong();
+
+    private final List<Employee> employees = Collections.synchronizedList(new ArrayList<>(List.of(
+        new Employee(idGenerator.incrementAndGet(), "John Doe"),
+        new Employee(idGenerator.incrementAndGet(), "Jack Doe")
+    )));
+
+    public List<EmployeeDto> listEmployees(Optional<String> prefix) {
+        List<Employee> filtered = employees.stream()
+                .filter(e -> prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
+                .collect(Collectors.toList());
+        return employeeMapper.toDto(filtered);
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        return employeeMapper.toDto(employees.stream()
+                .filter(e -> e.getId() == id).findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id)));
+    }
+
+    public EmployeeDto createEmployee(CreateEmployeeCommand command) {
+        Employee employee = new Employee(idGenerator.incrementAndGet(), command.getName());
+        employees.add(employee);
+        return employeeMapper.toDto(employee);
+    }
+
+    public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
+        Employee employee = employees.stream()
+                .filter(e -> e.getId() == id)
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id));
+        employee.setName(command.getName());
+        return employeeMapper.toDto(employee);
+    }
+
+    public void deleteEmployee(long id) {
+        Employee employee = employees.stream()
+                .filter(e -> e.getId() == id)
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id));
+        employees.remove(employee);
+    }
+}
